00f2bb5ad45c4dfad425ae2c286ef6f166ae771d
Bogdan <presnescubogdan98@gmail.com>
2025-06-26 12:39:37 +0300

    env keys

--- frontend/api/lib/getUserWithRole.js
+++ frontend/api/lib/getUserWithRole.js
@@ -2,8 +2,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY
 );
 
 module.exports = async function getUserWithRole(req, res, next) {
