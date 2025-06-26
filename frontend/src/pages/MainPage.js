a3e09b3f4d3512fcfd399cb249c726e4f8ec24ec
Robert Balacescu <robert.balacescu1@gmail.com>
2025-06-26 13:40:38 +0300

    update

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -133,7 +133,7 @@ const MainPage = ({ user })  => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${session.access_token}`
+          // Authorization: `Bearer ${session.access_token}`
         },
         body: JSON.stringify(newRoute)
       });
